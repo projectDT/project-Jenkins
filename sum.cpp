161ee762c3d337b61c34f7ba842f85200df3fc18
Team DT <79621444+projectDT@users.noreply.github.com>
2021-08-20 21:11:47 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -6,14 +6,13 @@ int main()
 	cout<<"Number of terms to add -> "<<n;
 	int arr[n];
 	cout<<"\nThose Numbers are :- "<<endl<<"100"<<endl<<"200"<<endl<<"300"<<endl<<"400"<<endl<<"500";
-	arr[0] = 100; arr[1] = 200; arr[2] = 300;
-    arr[3] = 400; arr[4] = 500;
+	arr[0] = 100; arr[1] = 200; arr[2] = 300; arr[3] = 400; arr[4] = 500;
 	
 	for(int i=0;i<n;i++)
-		sum+=arr[i];
+	{sum+=arr[i];}
 	
 	cout<<"\nTotal Sum is = "<<sum;
-    cout<<"\n\n\n\nProgram ends here ........"<<"\n\n";
+    	cout<<"\n\n\n\nProgram ends here ........"<<"\n\n";
 
 	return 0;
 }
